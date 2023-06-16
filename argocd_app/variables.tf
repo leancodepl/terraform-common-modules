70d147ca0bee2bc3afc809273c8574b64b6f6026
Marcin Dardziński <marcin.dardzinski@leancode.pl>
2023-06-16 09:10:14 +0200

    Add argocd app module

---
+++ argocd_app/variables.tf
@@ -0,0 +1,31 @@
+variable "repository" {
+  type = object({
+    url      = string,
+    revision = string,
+    path     = string,
+  })
+}
+
+variable "argocd_namespace" {
+  type    = string
+  default = "argocd"
+}
+
+variable "enable_auto_sync" {
+  type = bool
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "app_name" {
+  type = string
+}
+
+variable "cluster" {
+  type = object({
+    url       = string
+    namespace = string
+  })
+}
