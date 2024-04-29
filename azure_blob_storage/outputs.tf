c7c54328ab3837f8aed1ed5a684570defa2b32ba
Łukasz Garstecki <lukasz.garstecki@leancode.pl>
2024-04-29 12:47:12 +0200

    Add storage_account_access_key to azure_blob_storage outputs

--- azure_blob_storage/outputs.tf
+++ azure_blob_storage/outputs.tf
@@ -14,6 +14,10 @@ output "storage_connection_string" {
   value = azurerm_storage_account.storage.primary_blob_connection_string
 }
 
+output "storage_account_access_key" {
+  value = azurerm_storage_account.storage.primary_access_key
+}
+
 output "storage_containers" {
   value = azurerm_storage_container.containers
 }
