d11b6c802f07eb7c50adb6ad5eed7b46241195d9
Marcin Dardziński <marcin.dardzinski@leancode.pl>
2024-10-02 10:37:33 +0200

    Remove firebase project location from firebase module
    google_firebase_project_location is deprecated in favor of explicitly configuring google_app_engine_application and google_firestore_database. This resource will be removed in the next major release of the provider.

--- firebase/main.tf
+++ firebase/main.tf
@@ -18,15 +18,6 @@ resource "google_firebase_project" "project" {
   project  = var.gcp_project_id
 }
 
-resource "google_firebase_project_location" "location" {
-  count = var.location != null ? 1 : 0
-
-  provider    = google-beta
-  project     = var.gcp_project_id
-  location_id = var.location
-}
-
-
 resource "google_project_iam_member" "firebase_admins" {
   for_each = var.firebase_admins_emails
 
