07cb0a20dcfc62fa8c54159413968d50a3bd9df1
Emil Dragańczuk <emil.draganczuk@leancode.pl>
2025-01-29 09:15:39 +0100

    Add MSSQL Server SA password as output

--- mssql_server/outputs.tf
+++ mssql_server/outputs.tf
@@ -6,6 +6,11 @@ output "mssql_server_fqnd" {
   value = azurerm_mssql_server.main.fully_qualified_domain_name
 }
 
+output "mssql_server_sa_password" {
+  value     = random_password.db_sa.result
+  sensitive = true
+}
+
 output "mssql_databases" {
   value = azurerm_mssql_database.databases
 }
